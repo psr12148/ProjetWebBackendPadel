55f5c95d1f484ac0c4514f3f4b33a48f4feba340
emile <psr12148@students.ephec.be>
2026-04-06 11:40:32 +0200

    init

---
+++ src/main/java/be/ephec/pdw/projetwebbackendpadel/ProjetWebBackendPadelApplication.java
@@ -0,0 +1,13 @@
+package be.ephec.pdw.projetwebbackendpadel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetWebBackendPadelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetWebBackendPadelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/ephec/pdw/projetwebbackendpadel/ProjetWebBackendPadelApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ephec.pdw.projetwebbackendpadel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetWebBackendPadelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
